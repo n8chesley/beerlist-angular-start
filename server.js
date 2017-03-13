a895845d7ed3712e3440cdb932543438855e3c85
nate chesley <nate@almanac.local>
2017-03-13 19:09:10 +0200

    update server

--- server.js
+++ server.js
@@ -29,21 +29,36 @@ app.get('/', function(req, res, next) {
   res.send('Testing Server')
 })
 
-app.get('/beers', function(req, res, next) {
-  res.json({
-    beers: [{
-      name: '512 IPA',
-      style: 'IPA',
-      image_url: 'http://bit.ly/1XtmB4d',
-      abv: 5
-    }, {
-      name: '512 Pecan Porter',
-      style: 'Porter',
-      image_url: 'http://bit.ly/1Vk5xj4',
-      abv: 4
-    }]
+app.get('/beers', function (req, res, next) {
+  Beer.find(function (error, beers) {
+    if (error) {
+      console.error(error)
+      return next(error);
+    } else {
+      res.send(beers);
+    }
   });
 });
+
+app.post('/beers', function (req, res, next) {
+  res.send(req.body);
+});
+
+// app.get('/beers', function(req, res, next) {
+//   res.json({
+//     beers: [{
+//       name: '512 IPA',
+//       style: 'IPA',
+//       image_url: 'http://bit.ly/1XtmB4d',
+//       abv: 5
+//     }, {
+//       name: '512 Pecan Porter',
+//       style: 'Porter',
+//       image_url: 'http://bit.ly/1Vk5xj4',
+//       abv: 4
+//     }]
+//   });
+// });
 
 app.listen('8000', function() {
   console.log("Hark! What ho on 8000?");
