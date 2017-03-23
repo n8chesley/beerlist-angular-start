e6397c0dd59af319ac4715d324fd6a09cb4f86b5
nate chesley <nate@almanac.local>
2017-03-23 18:07:15 +0200

    add angular-ui-router module, start of config

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,15 @@
-var app = angular.module('beerList', []);
+var app = angular.module('beerList', ['ui.router']);
+
+app.config(['$stateProvider','$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/templates/home.html'
+    })
+    .state('beer', {
+      url: '/beers/:id',
+      templateUrl: '/templates/beer.html',
+    });
+
+  $urlRouterProvider.otherwise('/home');
+}]);
